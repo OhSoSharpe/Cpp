42b88b8f447eb300ab3b041bac73865b0d53ccd8
OhSoSharpe <96155547+OhSoSharpe@users.noreply.github.com>
2021-12-24 18:14:34 -0800

    Add files via upload

---
+++ AlertSystem.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+/*
+void nextBit(int[] array)
+{
+
+}
+
+int main()
+{
+    while (true)
+    {
+        int state = 0; //How we keep track of our state
+        int bit[] = { 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0 }; //An array of inputs to run through to see if we need to alert an engineer
+       
+
+         //The start of a FSM
+        switch (state)
+        {
+            case 0:
+             
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        }
+    }
+}
+*/
+while (true)
+{
+    int value;
+    cout << "Enter next digit in the sequence" << endl;
+    cin >> value >> endl;
+    if (value = 0) 
+    {
+        cout << "Enter next digit in the sequence" << endl;
+        cin >> value >> endl;
+        if (value = 1)
+        {
+            cout << "Enter next digit in the sequence" << endl;
+            cin >> value >> endl;
+            if (value = 0)
+            {
+                cout << "Enter next digit in the sequence" << endl;
+                cin >> value >> endl;
+                if (value = 1)
+                {
+                    cout << "Enter next digit in the sequence" << endl;
+                    cin >> value >> endl;
+                    if (value = 1)
+                    {
+                        cout << "Enter next digit in the sequence" << endl;
+                        cin >> value >> endl;
+                        if (value = 0)
+                        {
+                            cout << "Data sequence 010110 has been achieved!" << endl;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else    
+}
